731544f735dc627a434b9088f062fd918ab4320f
Riley Brown <riley@riley.gg>
2019-11-28 16:37:49 -0500

    added basic styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 import useRecordMicrophone from 'Hooks/useRecordMicrophone';
 
+import micIcon from './mic.svg';
+
+import './App.css';
+
 export default function App() {
   const onStartSpeaking = () => {
-    console.log('started speaking');
+    console.log('started speaking eriuflhui4tlk');
   };
 
   const onStoppedSpeaking = () => {
@@ -26,14 +30,35 @@ export default function App() {
   });
 
   if (error) {
-    return <h1>{error}</h1>;
+    return (
+      <div
+        style={{
+          maxWidth: '600px',
+          margin: '100px auto',
+          textAlign: 'center'
+        }}
+      >
+        <p>
+          {error}
+          <span style={{ fontSize: '3rem' }}>🤷‍</span>
+        </p>
+      </div>
+    );
   }
 
   return (
-    <div>
-      <h1 onClick={isRecording ? stopCapturing : startCapturing}>
-        Recording: {isRecording.toString()}
-      </h1>
+    <div
+      style={{
+        maxWidth: '600px',
+        margin: '100px auto',
+        textAlign: 'center'
+      }}
+    >
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopCapturing : startCapturing}>
+        <span>{isRecording ? 'Stop Recording' : 'Start Recording'}</span>
+        <img data-recording={isRecording} src={micIcon} alt="" />
+      </button>
       <ul>
         {results.map(result => (
           <li>{result}</li>
